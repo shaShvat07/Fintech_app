8290b4bfedcf2136e2df45df500cb3415ca032ff
Shashvat <shashvatpatel2803@gmail.com>
2023-06-04 14:50:55 +0530

    Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () =>  (
+    <div className="bg-primary w-full overflow-hidden">
+        <h1> Hello there!! </h1>
+    </div>
+  );
+
+
+export default App
