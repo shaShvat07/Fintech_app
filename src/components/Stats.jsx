7bd92b49f283a4770c1881ecb3f534b11f812d41
Shashvat <shashvatpatel2803@gmail.com>
2023-06-05 11:29:24 +0530

    Added hero business header Get started stats section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { stats } from '../constants'
+import styles  from '../style'
+const Stats = () =>  (
+    <section className={`${styles.flexCenter} flex-row
+    flex-wrap sm:mb-20 mb-6`}>
+        {stats.map((stat) => (
+          <div key={stat.id} className={`flex-1 
+          flex justify-start items-center flex-row m-3`}>
+            <h4 className='font-poppins text-white
+             font-semibold xs:text-[40px] text-[30px] 
+             xs:leading-[53px] leading-[43px]'>
+              {stat.value}
+            </h4>
+            <p className='font-poppins text-gradient
+             font-normal xs:text-[20px] text-[15px] 
+             xs:leading-[26px] leading-[21px] uppercase
+             ml-3'> {stat.title} </p>
 
-const Stats = () => {
-  return (
-    <div>Stats</div>
+          </div>
+        ))}
+    </section>
   )
-}
+
 
 export default Stats
